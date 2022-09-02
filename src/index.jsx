b0d5046c79faa0c8c091a1b591181ced115f0bc8
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-02 10:36:34 +0200

    Basic app example

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <h1>
+        Lesson 2 - Movie database,
+        {document.getElementById("app")}
+    </h1>
+);
