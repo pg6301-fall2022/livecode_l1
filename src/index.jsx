db3b55b46c43121a4cead4c57714efb25ad68bf5
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-02 13:17:42 +0200

    Step 6 - actually use the state to create a new movie

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import {useState} from "react";
 import * as ReactDOM from "react-dom/client";
-import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -9,19 +10,6 @@ const MOVIES = [
         title: "Dune",
         plot: " The desert world of Arrakis, source of all Melange, is fought over by two houses of the Imperium ",
         year: 2021
-    },
-    {
-        title: "Plan 9 from outer space",
-        plot: "I dunno mate... Bela Lugosi is in it, though...",
-        year: 1957
-    }
-];
-
-const BAD_MOVIES = [
-    {
-        title: "Enter the Ninja",
-        plot: " Guy in jeans is more ninja than the ninjas ",
-        year: 1981
     },
     {
         title: "Plan 9 from outer space",
@@ -45,21 +33,49 @@ function ListMovies({movies}){
         <h1> List Movies </h1>
         {
             movies.map( m =>
-            <>
-                <h2>{m.title} - {m.year}</h2>
-                <div>
-                    {m.plot}
-                </div>
-            </>
+            <div key={m.title}>
+                <h2>{m.title} - ({m.year})</h2>
+                <div> {m.plot} </div>
+            </div>
         )}
     </div>
+}
+
+
+function NewMovie({onAddMovie}){
+    const [title, setTitle] = useState("");
+    const [year, setYear]  = useState(0);
+    const [plot, setPLot]  = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(MOVIES);
+        onAddMovie({title, plot, year});
+        navigate("/");
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <h1> Enter details for new Movie </h1>
+        <div>
+            <label> Title: <input value={title} onChange={e => setTitle(e.target.value)} /></label>
+        </div>
+        <div>
+            <label> Year: <input value={year} onChange={e => setYear(e.target.value)} /></label>
+        </div>
+        <div>
+            <label> Plot: <textarea value={plot} onChange={e => setPLot(e.target.value)}/></label>
+        </div>
+        <button>Submit</button>
+    </form>
 }
 
 function Application(){
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<FrontPage/>}></Route>
-            <Route path="/movies/new" element={<h1>New movie</h1>}></Route>
+            <Route path="/movies/new" element={<NewMovie onAddMovie={m => MOVIES.push(m)}/>}></Route>
             <Route path="/movies" element={<ListMovies movies={MOVIES}/>}></Route>
         </Routes>
     </BrowserRouter>
