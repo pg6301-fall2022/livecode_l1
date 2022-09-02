2ccdf6ce3ec5cb6ed36ae49cd470a28f3ac6c5c0
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-02 12:14:06 +0200

    Step 3 - basic routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,29 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function FrontPage(){
+    return <div>
+        <h1> Lesson 2 - Movie database</h1>
+        <ul>
+            <li><Link to="/movies"> List Movies </Link></li>
+            <li><Link to="movies/new"> New Movie </Link></li>
+        </ul>
+    </div>
+}
+
+function Application(){
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<FrontPage/>}></Route>
+            <Route path="/movies/new" element={<h1>New movie</h1>}></Route>
+            <Route path="/movies" element={<h1>List movies</h1>}></Route>
+        </Routes>
+    </BrowserRouter>
+}
 
 root.render(
-    <h1>
-        Lesson 2 - Movie database,
-        {document.getElementById("app")}
-    </h1>
+    <Application/>
 );
