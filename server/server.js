1b4e6a04952783e76d93dc5649e93d7c366dac8f
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-02 13:55:24 +0200

    Step 8 - basic server and moving the data to that server.

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express  from "express";
+import path from "path";
+
+const app = express();
+
+const MOVIES = [
+    {
+        title: "Dune",
+        plot: " The desert world of Arrakis, source of all Melange, is fought over by two houses of the Imperium ",
+        year: 2021
+    },
+    {
+        title: "Plan 9 from outer space",
+        plot: "I dunno mate... Bela Lugosi is in it, though...",
+        year: 1957
+    }
+];
+
+app.get("/api/movies", (req, res) =>{
+    res.json(MOVIES);
+});
+
+app.use(express.static(path.resolve("../dist")));
+
+const server = app.listen(3000, () => {
+    console.log("Listening on http://localhost:" + server.address().port);
+})
--- src/index.jsx
+++ src/index.jsx
@@ -5,19 +5,6 @@ import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const MOVIES = [
-    {
-        title: "Dune",
-        plot: " The desert world of Arrakis, source of all Melange, is fought over by two houses of the Imperium ",
-        year: 2021
-    },
-    {
-        title: "Plan 9 from outer space",
-        plot: "I dunno mate... Bela Lugosi is in it, though...",
-        year: 1957
-    }
-];
-
 function FrontPage(){
     return <div>
         <h1> Lesson 2 - Movie database</h1>
